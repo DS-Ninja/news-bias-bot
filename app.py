fa974c4db9fae24f03d29d078f33c0d8a3e13ae7
Dmitrij Levyj <ld.scorpion@gmail.com>
2026-02-08 20:25:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1939,10 +1939,9 @@ def dashboard():
 
     <!-- TICKERS -->
     <div class="block">
-      <div class="blockTitle">TICKERS</div>
+      <div class="blockTitle">TICKERS | HEADLINES (latest)</div>
       <div class="tickerstack">
         <div>
-          <div class="ticklabel">HEADLINES (latest)</div>
           <div class="tickerline marquee"><div class="marqueeInner" id="marqueeNews"></div></div>
         </div>
         <div>

ade7e427cc62d3061620493dd3fd3f10dff404d2
Dmitrij Levyj <ld.scorpion@gmail.com>
2026-02-08 08:15:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1656,7 +1656,7 @@ def dashboard():
     }
     .marquee:hover .marqueeInner{animation-play-state:paused;}
     
-    #marqueeNews{ animation-duration: 80s; }   /* HEADLINES в 2 раза медленнее */
+    #marqueeNews{ animation-duration: 130s; }   /* HEADLINES в 2 раза медленнее */
     
     @keyframes scroll{
       0%{transform:translateX(0);}
